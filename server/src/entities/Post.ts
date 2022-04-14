a4df934c34f010799a49750bcd0f6739f1196dd4
Lucas Sellari <lucasmiguel448@gmail.com>
2022-04-14 19:28:36 -0300

    implementado crud básico com graphql e mikroORM

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,18 +1,24 @@
 import { Entity, OptionalProps, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
   [OptionalProps]?: "id" | "title" | "updateAt" | "createdAt";
   //4 colunas
+  @Field(() => Int)
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: "date" })
   createdAt: Date = new Date();
 
+  @Field(() => String)
   @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt: Date = new Date();
 
+  @Field(() => String)
   @Property({ type: "text" })
   title!: string;
 }
